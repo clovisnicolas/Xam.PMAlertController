f50f9b7c60bbe743df5ecc1c41f2ee76ea173e39
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-04-14 09:57:30 +0200

    add working sample

--- Xam.PMAlertController.Sample/ViewController.cs
+++ Xam.PMAlertController.Sample/ViewController.cs
@@ -14,7 +14,40 @@ namespace Xam.PMAlertController.Sample
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
+			this.twoChoicesButton.TouchUpInside += TwoChoicesButton_TouchUpInside;
+			this.threeChoicesButton.TouchUpInside += ThreeChoicesButton_TouchUpInside;
+
 			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		void TwoChoicesButton_TouchUpInside(object sender, EventArgs e)
+		{
+			var alertVC = new PMAlertController("Welcome",
+												"This is a sample for PMAlertController with two choices available",
+												UIImage.FromBundle("home"),
+												PMAlertControllerStyle.Walkthrough);
+
+			alertVC.AddAction(new PMAlertAction("Cool", PMAlertActionStyle.Default, null));
+			alertVC.AddAction(new PMAlertAction("I don't care", PMAlertActionStyle.Cancel, null));
+
+			this.PresentViewController(alertVC, true, null);
+		}
+
+		void ThreeChoicesButton_TouchUpInside(object sender, EventArgs e)
+		{
+			var alertVC = new PMAlertController("Welcome back",
+												"This is a sample for PMAlertController with more than two choices available",
+												UIImage.FromBundle("home"),
+												PMAlertControllerStyle.Walkthrough);
+
+			var customAction = new PMAlertAction("Awesome", PMAlertActionStyle.Default, null);
+			customAction.SetTitleColor(UIColor.FromRGB(80, 190, 135), UIControlState.Normal);
+			alertVC.AddAction(customAction);
+			alertVC.AddAction(new PMAlertAction("Cool", PMAlertActionStyle.Default, null));
+			alertVC.AddAction(new PMAlertAction("I don't care", PMAlertActionStyle.Default, null));
+			alertVC.AddAction(new PMAlertAction("Cancel", PMAlertActionStyle.Cancel, null));
+
+			this.PresentViewController(alertVC, true, null);
 		}
 
 		public override void DidReceiveMemoryWarning()
--- Xam.PMAlertController.Sample/ViewController.designer.cs
+++ Xam.PMAlertController.Sample/ViewController.designer.cs
@@ -1,17 +1,34 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace Xam.PMAlertController.Sample
 {
-	[Register("ViewController")]
+	[Register ("ViewController")]
 	partial class ViewController
 	{
-		void ReleaseDesignerOutlets()
+		[Outlet]
+		UIKit.UIButton threeChoicesButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton twoChoicesButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
 		{
+			if (twoChoicesButton != null) {
+				twoChoicesButton.Dispose ();
+				twoChoicesButton = null;
+			}
+
+			if (threeChoicesButton != null) {
+				threeChoicesButton.Dispose ();
+				threeChoicesButton = null;
+			}
 		}
 	}
 }
